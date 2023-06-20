056cf4eebaddc8d9bdc43a596cf99e5f92beec59
Fxkhalil0 <118560438+Fxkhalil0@users.noreply.github.com>
2023-06-19 18:15:38 -0800

    ...

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -7,12 +7,8 @@ const trips = require("../Models/trip");
 const boatOwner = require("../Models/boatOwner");
 const reviews = require("../Models/review");
 const Comments = require("../Models/userComments");
-<<<<<<< Updated upstream
 const mongoose = require('mongoose');
 const { ObjectId } = require('mongoose').Types;
-=======
-const ObjectId = require("mongodb").ObjectID;
->>>>>>> Stashed changes
 const cookieParser = require("cookie-parser");
 const cors = require("cors")
 const jwt = require("jsonwebtoken");
@@ -132,20 +128,14 @@ route.get("/editUserinfo", async function (req, res) {
 route.put("/editUserinfo/:id",
   // upload.single("img"),
   async function (req, res) {
-<<<<<<< Updated upstream
     console.log("dddd")
   let data ;
     console.log(req.body,"BODY")
-=======
-  // console.log(req.file)
-    console.log(req.body.img,"BODY")
->>>>>>> Stashed changes
     console.log(req.params.id)
     let editUserinfo = await user.findByIdAndUpdate(req.params.id, {
       name: req.body.name,
       // address:req.body.address,
       phone:req.body.phone,
-<<<<<<< Updated upstream
        img: req.file.filename,
     }).then((res)=>{
       console.log(res,"dsada");
@@ -153,13 +143,6 @@ route.put("/editUserinfo/:id",
     })
   
     res.send(data);
-=======
-      //  img: req.file.name,
-    });
-    let userData = await user.findById(req.params.id)
-    // console.log(userData,"data")
-    res.send(userData);
->>>>>>> Stashed changes
   });
 
 //get all boats
@@ -445,7 +428,7 @@ route.get('/userTrips/finished/:id', async (req, res) => {
 route.get('/userTrips/pending/:id', async (req, res) => {
   // const userId = new mongoose.Types.ObjectId(req.params.id);
   console.log(req.params.id)
-  let id=new ObjectId( req.params.id)
+  let id=new ObjectId(req.params.id)
   console.log(id)
   const userTrips = await trips.find({$and:[ {clientId:id},{ status: "pending"}] }).populate("boatId")
   console.log(userTrips)
