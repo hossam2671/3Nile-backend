cbf83f8c325c7f35ad32efb9388b64fb21c9c73b
hossam2671 <108755474+hossam2671@users.noreply.github.com>
2023-06-21 11:04:02 +0300

    ....

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -7,12 +7,7 @@ const trips = require("../Models/trip");
 const boatOwner = require("../Models/boatOwner");
 const reviews = require("../Models/review");
 const Comments = require("../Models/userComments");
-<<<<<<< Updated upstream
-const mongoose = require('mongoose');
-const { ObjectId } = require('mongoose').Types;
-=======
 const ObjectId = require("mongodb").ObjectID;
->>>>>>> Stashed changes
 const cookieParser = require("cookie-parser");
 const cors = require("cors")
 const jwt = require("jsonwebtoken");
@@ -132,34 +127,19 @@ route.get("/editUserinfo", async function (req, res) {
 route.put("/editUserinfo/:id",
   // upload.single("img"),
   async function (req, res) {
-<<<<<<< Updated upstream
     console.log("dddd")
   let data ;
     console.log(req.body,"BODY")
-=======
-  // console.log(req.file)
-    console.log(req.body.img,"BODY")
->>>>>>> Stashed changes
     console.log(req.params.id)
     let editUserinfo = await user.findByIdAndUpdate(req.params.id, {
       name: req.body.name,
       // address:req.body.address,
       phone:req.body.phone,
-<<<<<<< Updated upstream
-       img: req.file.filename,
-    }).then((res)=>{
-      console.log(res,"dsada");
-      data= res
-    })
-  
-    res.send(data);
-=======
       //  img: req.file.name,
     });
     let userData = await user.findById(req.params.id)
     // console.log(userData,"data")
     res.send(userData);
->>>>>>> Stashed changes
   });
 
 //get all boats
